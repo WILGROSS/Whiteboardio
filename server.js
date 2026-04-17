b7ab86fc1e376add0913248f2b0126a1e31e1f05
WilliamGross <william@gross.nu>
2026-04-17 22:19:37 +0200

    Serve static files and add frontend scaffold

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from Whiteboardio!');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
