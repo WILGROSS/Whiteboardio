824f5096563f434af6091e0f75c18a29652aa108
WilliamGross <william@gross.nu>
2026-04-17 22:19:16 +0200

    Add Express for routing

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const http = require('http');
+const express = require('express');
 
+const app = express();
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Whiteboardio!\n');
+app.get('/', (req, res) => {
+  res.send('Hello from Whiteboardio!');
 });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
