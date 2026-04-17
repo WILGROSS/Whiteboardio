a14f9d604f072169fb9ccb94b817c617a705669e
WilliamGross <william@gross.nu>
2026-04-17 22:32:21 +0200

    Add WebSocket server

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const { WebSocketServer } = require('ws');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
