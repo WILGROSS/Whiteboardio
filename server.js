c51cec44b1e6a8d615a6ecd9f7be24e9bfb91926
WilliamGross <william@gross.nu>
2026-04-18 12:55:07 +0200

    Use dynamic PORT for deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const { WebSocketServer } = require('ws');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
